28c9f3df218afbe12d7212e35c999828a577e89c
Md. Shafiul Islam Sohan <sohan110111@gmail.com>
2022-06-15 10:55:01 +0600

    Array in javaScript

--- CollectionOfValue.js
+++ CollectionOfValue.js
@@ -9,3 +9,23 @@ console.log(array[indexOfTwo + 1])
 array.push(4)
 
 console.log(array)
+
+const Animals = ["cat", "dog", "cow", "tiger", "monkey", "snake", "duck"]
+console.log("Length of Array is : ", Animals.length)
+
+console.log(Animals[5])
+
+const multiDimensionalArray = [
+    [1, 2, 3, 4],
+    [4, 5, 6],
+    [7, 8, 9]
+]
+
+console.log(multiDimensionalArray)
+console.log(multiDimensionalArray[0][0])
+console.log(multiDimensionalArray.length)
+console.log(multiDimensionalArray[0].length)
+
+
+//Objects (hash tables)
+
