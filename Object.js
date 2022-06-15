a4ec4535c1517c564f26b1005f76188b4bfc01c7
Md. Shafiul Islam Sohan <sohan110111@gmail.com>
2022-06-15 18:35:10 +0600

    ForEach in Object

--- Object.js
+++ Object.js
@@ -10,3 +10,11 @@ person.country = 'United States'
 console.log(person)
 let a = "age"
 console.log(person[a])
+
+let text = ''
+
+for (let x in person) {
+    text += person[x]+" "
+
+}
+console.log(text)
