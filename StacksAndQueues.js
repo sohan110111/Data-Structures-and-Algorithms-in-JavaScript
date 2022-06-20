dedd7d8eb0f2e16cf425af7f88885bf415fc7bd2
Md. Shafiul Islam Sohan <sohan110111@gmail.com>
2022-06-20 14:49:56 +0600

    Queues with class

--- StacksAndQueues.js
+++ StacksAndQueues.js
@@ -32,3 +32,51 @@ stack.pop()
 console.log(stack);
 
 console.log(stack.peek());
+
+
+// now queue using array method
+const queue = [];
+
+
+// queue 
+queue.push("JackFruit")
+queue.push("Mango")
+queue.push("Banana")
+
+console.log(queue)
+//dequeue
+queue.shift()
+console.log(queue)
+console.log(queue.shift())
+console.log(queue)
+
+// queue with javaScript Class
+class Queue {
+    constructor() {
+        this.storage = {}
+        this.head = 0
+        this.tail = 0
+    }
+    // queue
+    enqueue(element1) {
+        this.storage[this.tail] = element1
+        this.tail++
+    }
+
+    //dequeue
+    dequeue() {
+        let removed = this.storage[this.head]
+        this.head++
+        return removed;
+    }
+}
+
+const queueObject = new Queue();
+
+queueObject.enqueue("book1")
+queueObject.enqueue("book2")
+queueObject.enqueue("book3")
+
+console.log(queueObject)
+
+
